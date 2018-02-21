ea73aa49f7487f1f5024c3abedab33a6cfd30699
Dave Shanley <dshanley@vmware.com>
2018-02-21 12:51:02 -0500

    trying to work in toast.

--- testApp/src/app/app.component.ts
+++ testApp/src/app/app.component.ts
@@ -9,9 +9,22 @@ import {MessagebusService, StompService} from "@vmw/bifrost";
 export class AppComponent {
     title = 'app';
 
+    toasts = [];
+    fade: boolean = true;
+
     constructor(private bus: MessagebusService) {
         this.bus.api.enableMonitorDump(true);
         this.bus.api.silenceLog(false);
+        this.toasts = [];
+    }
+
+    listenForAlerts(): void {
+        this.bus.listenStream('app-alerts')
+            .handle(
+                (payload) => {
+                    this.toasts.push({fade: true, error: false, title: 'pop', description: 'fishes', date: Date.now(), link: 'http://www.google.com'});
+                }
+            );
     }
 
 }
