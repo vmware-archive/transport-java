b6580f8c9bcca7630f33dcdbc78168ed3db25421
Dave Shanley <dshanley@vmware.com>
2019-02-09 21:36:33 -0500

    [appfab-ui] Added in build detailed and new logo

---
+++ sample-ui/src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "3446d2b-dirty",
+    "hash": "3446d2b",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "3446d2b-dirty",
+    "semverString": null,
+    "version": "0.0.0"
+};
+/* tslint:enable */
