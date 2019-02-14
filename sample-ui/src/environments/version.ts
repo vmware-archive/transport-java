21894b76afbf1123cc268d1df1a9919efe0410c9
Dave Shanley <dshanley@vmware.com>
2019-02-14 02:09:00 -0500

    [appfab-ui]
    Updated to alpha version of 0.13.0

--- sample-ui/src/environments/version.ts
+++ sample-ui/src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "a2885d6-dirty",
-    "hash": "a2885d6",
+    "dirty": false,
+    "raw": "c31fe8b",
+    "hash": "c31fe8b",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "a2885d6-dirty",
+    "suffix": "c31fe8b",
     "semverString": null,
     "version": "0.0.3",
-    "time": "2019-02-14T06:59:26.859Z"
+    "time": "2019-02-14T07:05:40.381Z"
 };
 /* tslint:enable */
