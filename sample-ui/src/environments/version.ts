f277eadaf13d73f35cbf1db43eb12c6adc29867f
Dave Shanley <dshanley@vmware.com>
2019-02-14 15:10:47 -0500

    [appfab-ui] New docker-compose file.
    Josh to take over!

--- sample-ui/src/environments/version.ts
+++ sample-ui/src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "c31fe8b",
-    "hash": "c31fe8b",
+    "dirty": true,
+    "raw": "21894b7-dirty",
+    "hash": "21894b7",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "c31fe8b",
+    "suffix": "21894b7-dirty",
     "semverString": null,
     "version": "0.0.3",
-    "time": "2019-02-14T07:05:40.381Z"
+    "time": "2019-02-14T20:04:25.107Z"
 };
 /* tslint:enable */
