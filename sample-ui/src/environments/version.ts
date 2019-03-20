0c31b92f514e702c43c28875c2ec1de38b051dbc
Dave Shanley <dshanley@vmware.com>
2019-03-20 07:14:39 -0400

    [appfabric-ui] updated to the latest version of TS bifrost

--- sample-ui/src/environments/version.ts
+++ sample-ui/src/environments/version.ts
@@ -2,14 +2,14 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "48d338f-dirty",
-    "hash": "48d338f",
+    "raw": "b93854c-dirty",
+    "hash": "b93854c",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "48d338f-dirty",
+    "suffix": "b93854c-dirty",
     "semverString": null,
     "version": "0.0.11",
-    "time": "2019-03-20T03:46:14.957Z"
+    "time": "2019-03-20T11:08:34.059Z"
 };
 /* tslint:enable */
