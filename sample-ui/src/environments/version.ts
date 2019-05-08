ebb386e902d3d2f9a1613009a9e5105a65f77cb3
Dave Shanley <dshanley@vmware.com>
2019-05-08 00:52:54 -0400

    [sample-ui] Added new snazzy fabric videos to docs

--- sample-ui/src/environments/version.ts
+++ sample-ui/src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "be1acf0-dirty",
-    "hash": "be1acf0",
+    "dirty": false,
+    "raw": "9edecf3",
+    "hash": "9edecf3",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "be1acf0-dirty",
+    "suffix": "9edecf3",
     "semverString": null,
-    "version": "0.0.11",
-    "time": "2019-03-25T00:59:26.342Z"
+    "version": "0.0.12",
+    "time": "2019-05-08T04:23:27.324Z"
 };
 /* tslint:enable */
