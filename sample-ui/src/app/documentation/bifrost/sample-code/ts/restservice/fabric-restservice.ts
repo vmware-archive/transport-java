23a9f17569f9452d768f3df8e53ab6670fc62bcf
Dave Shanley <dshanley@vmware.com>
2019-05-09 18:47:38 -0400

    Working through fabric rest service code

---
+++ sample-ui/src/app/documentation/bifrost/sample-code/ts/restservice/fabric-restservice.ts
@@ -0,0 +1,71 @@
+/**
+ * Copyright(c) VMware Inc. 2019
+ */
+import { AbstractBase, HttpRequest, RestError } from '@vmw/bifrost/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MyAPIServiceChannel, MyAPIServiceRequest, MyAPIServiceResponse } from './myapi.service.model';
+import { ClrLoadingState } from '@clr/angular';
+import { RestService } from '@vmw/bifrost/core/services/rest/rest.service';
+import { ServiceLoader } from '@vmw/bifrost/util/service.loader';
+import { GeneralUtil } from '@vmw/bifrost/util/util';
+
+@Component({
+    selector: 'fabric-restservice-component',
+    template: `
+        <button (click)="requestAPI()" class="btn btn-primary" [clrLoading]="iconState">Request API via Fabric Rest Service</button><br/>
+        Response: {{response}}`
+})
+export class FabricRestServiceComponent extends AbstractBase implements OnInit, OnDestroy {
+
+    public response = 'nothing yet, request something!';
+    public iconState: ClrLoadingState = ClrLoadingState.DEFAULT;
+    private restService: RestService;
+
+    constructor() {
+        super('FabricRestServiceComponent');
+    }
+
+
+
+    disableLocalRestService(): void {
+        this.restService.offline();
+        this.bus.markChannelAsGalactic('fabric-rest');
+        // get a reference to
+    }
+
+    enableLocalRestService(): void {
+        this.bus.markChannelAsLocal('fabric-rest');
+        this.restService.online();
+    }
+
+    ngOnInit(): void {
+        this.restService = ServiceLoader.getService(RestService);
+    }
+
+    ngOnDestroy(): void {
+       this.enableLocalRestService();
+    }
+
+    /**
+     * s
+     */
+    public requestAPI(): void {
+        this.iconState = ClrLoadingState.LOADING;
+        this.disableLocalRestService();
+
+
+        this.restServiceRequest(
+            {
+                id: GeneralUtil.genUUID(),
+                uri: `https://jsonplaceholder.typicode.com/todos/1`,
+                method: HttpRequest.Get,
+                successHandler: (response: any) => {
+                  this.log.info('hello kitty!' + response);
+                },
+                errorHandler: (error: RestError) => {
+                    this.log.error('bad kitty!' + error.message);
+                }
+            }
+        );
+    }
+}
--- sample-ui/src/environments/version.ts
+++ sample-ui/src/environments/version.ts
@@ -1,15 +1,15 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "1f218bd",
-    "hash": "1f218bd",
+    "dirty": true,
+    "raw": "d7a15d9-dirty",
+    "hash": "d7a15d9",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "1f218bd",
+    "suffix": "d7a15d9-dirty",
     "semverString": null,
     "version": "0.0.13",
-    "time": "2019-05-09T02:36:18.750Z"
+    "time": "2019-05-09T19:19:08.379Z"
 };
 /* tslint:enable */
