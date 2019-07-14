76e4d218bd02d6b98072d374b6b2c083b36648a5
Akmal Khan <akmalk@vmware.com>
2019-07-14 10:16:25 -0700

    ServGen specification documents: service.json

---
+++ sample-ui/src/app/documentation/sewing-machine/servgen-specs/service-json/service-json.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewChecked, Component, OnDestroy, OnInit } from '@angular/core';
+import { BaseDocsComponent } from '../../../base.docs.component';
+import { HighlightService } from '../../../../local-services/highlight.service';
+
+@Component({
+  selector: 'appfab-servgen',
+  templateUrl: './service-json.component.html',
+  styleUrls: ['./service-json.component.scss']
+})
+export class ServiceJsonComponent extends BaseDocsComponent implements OnInit, OnDestroy, AfterViewChecked {
+
+    constructor(private highlightService: HighlightService) {
+        super('ServGenSpecsServiceComponent');
+    }
+
+    ngOnInit() {
+        this.setSewingMachineDocsActive(true);
+    }
+
+    ngOnDestroy() {
+    }
+
+    ngAfterViewChecked() {
+        if (!this.highlighted) {
+            this.highlightService.highlightAll();
+            this.highlighted = true;
+        }
+    }
+
+}
--- sample-ui/src/environments/version.ts
+++ sample-ui/src/environments/version.ts
@@ -2,14 +2,14 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "b3fd2b3-dirty",
-    "hash": "b3fd2b3",
+    "raw": "b3cb7fe-dirty",
+    "hash": "b3cb7fe",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "b3fd2b3-dirty",
+    "suffix": "b3cb7fe-dirty",
     "semverString": null,
     "version": "0.1.3",
-    "time": "2019-07-12T23:31:55.378Z"
+    "time": "2019-07-14T17:23:21.291Z"
 };
 /* tslint:enable */
