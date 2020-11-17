a99c34f31a163790ea395377f451f62ccbe3b832
Dave Shanley <dshanley@vmware.com>
2020-11-17 17:47:05 -0500

    Added in multi module config, and added sample app.
    Ready to bring in example code.

---
+++ sample-app/src/main/java/app/Application.java
@@ -0,0 +1,18 @@
+package app;
+
+/*
+ * Copyright(c) VMware Inc. 2017-2018
+ */
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.vmware.transport.bridge.spring.config.annotation.EnableTransport;
+
+@SpringBootApplication
+@EnableTransport
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
